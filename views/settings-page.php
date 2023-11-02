47d1353d5afe92e31620e3957f029e712411ffc3
recypher-zz <kniemerg001@me.com>
2023-11-01 20:33:46 -0700

    Created SQL query and For loop to build selection menu based on tables in database, need to finish the rest of form submission

--- views/settings-page.php
+++ views/settings-page.php
@@ -1,9 +1,22 @@
 <div class="wrap">
     <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-    <form action="options.php">
+    <select name="select-table" id="apepi_SelectColumn">
         <?php
-        
+            global $wpdb;
+            $sql_query        = '%%';
+            $results          = $wpdb->get_results( $wpdb->prepare( 'SHOW TABLES LIKE %s', $sql_query ) );
+            $table_name_array = array();
 
+            foreach ( $results as $index => $value ) {
+                foreach ( $value as $table_name_temp ) {
+                    array_push( $table_name_array, $table_name_temp );
+                }
+            }
+
+            foreach ( $table_name_array as $table ) {
+                echo '<option value=' . esc_attr( $tb );
+                echo ' > ' . esc_html( $table ) . ' </option>';
+            }
         ?>
-    </form>
+    </select>
 </div>
