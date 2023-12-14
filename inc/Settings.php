d4d8b53dfb9a62d9948eee8990e7e99344f6c255
recypher-zz <kniemerg001@me.com>
2023-12-14 13:56:40 -0800

    Introduced a testing class called ReturnData to just pull data back as a GET.

--- inc/Settings.php
+++ inc/Settings.php
@@ -2,12 +2,14 @@
 namespace ApePI\Core;
 use ApePI\Core\Components\TableDropdown;
 use ApePI\Core\Components\ColumnDropdown;
+use ApePI\Core\Components\ReturnData;
 
 if( ! class_exists( 'Settings' ) ){
     class Settings{
 
         public function __construct() {
-            add_action( 'wp_ajax_columns', array($this, 'create_column_dropdown') );
+            add_action( 'wp_ajax_columns', array($this, 'create_column_dropdown' ) );
+            add_action( 'wp_ajax_returnData', array($this, 'show_return_data' ) );
         }
         
         public function create_table_dropdown() {
@@ -17,6 +19,11 @@ if( ! class_exists( 'Settings' ) ){
         public function create_column_dropdown() {
             $column_dropdown = new ColumnDropdown($_POST["table"]);
             wp_send_json($column_dropdown);
+        }
+
+        public function show_return_data() {
+            $return_data = new ReturnData($_POST["table"], $_POST["column"]);
+            wp_send_json($return_data);
         }
     }
 }
