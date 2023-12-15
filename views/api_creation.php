549fc57830d868609b4d526bf890d83b664d4422
recypher-zz <kniemerg001@me.com>
2023-12-15 15:03:38 -0800

    First test and creation of views within the ApePI plugin space.

---
+++ views/api_creation.php
@@ -0,0 +1,20 @@
+<?php
+
+function api_creation_screen($data){
+    ?>
+    <div class="endpoint_selector">
+        <?php $data; ?>
+        <div id="returned_columns">
+
+        </div>
+    </div>
+
+    <div>
+        <pre class="returned_data">
+
+        </pre>
+    </div>
+    <?php
+}
+
+?>
