0632e7b24d30e48742d5dc49fc4ce1749ba5b5c0
recypher-zz <kniemerg001@me.com>
2023-12-15 19:40:27 -0800

    Updtaed for new custom routes vriables.

--- views/api_creation.php
+++ views/api_creation.php
@@ -7,13 +7,28 @@ function api_creation_screen($data){
         <div id="returned_columns">
 
         </div>
+        <div>
+            <select class="method_type">
+                <option value="#"> Select a Method... </option>
+                <option value="GET">GET</option>
+                <option value="POST">POST</option>
+                <option value="PATCH">PATCH</option>
+                <option value="DELETE">DELETE</option>
+            </select>
+        </div>
+
+        <div>
+            <pre class="returned_data">
+
+            </pre>
+        </div>
+
+        <div>
+            <button class="submit_api">Submit</button>
+        </div>
     </div>
 
-    <div>
-        <pre class="returned_data">
-
-        </pre>
-    </div>
+    
     <?php
 }
 
