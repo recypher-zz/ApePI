376be151f3169cd079ff768b0140adc0a2718c28
recypher-zz <kniemerg001@me.com>
2023-12-19 20:25:53 -0800

    Created constructor for rest_api_init

--- inc/CustomRoute.php
+++ inc/CustomRoute.php
@@ -2,30 +2,33 @@
 namespace ApePI\Core;
 
 class CustomRoute{
-    public static function register_custom_routes() {
+
+    public function __construct(){
+        add_action('rest_api_init', array($this, 'register_custom_routes'));
+    }
+
+    public function register_custom_routes() {
         global $wpdb;
         $table_name = $wpdb->prefix . 'custom_routes';
         $routes = $wpdb->get_results("SELECT * FROM $table_name", ARRAY_A);
 
         foreach ($routes as $route) {
-            $table_name = $wpdb->prefix . 'custom_routes';
+            $table_name = $route['route_table'];
             $unserialize_columns = unserialize( $route['route_columns'] );
-            $columns_to_return = explode( ',', $unserialize_columns );
+            $columns_to_return = $unserialize_columns;
 
             register_rest_route('apepi/v1', '/' . $route['route_name'], array(
                 'methods' => $route['route_method'],
                 'callback' => function ( $data ) use ($table_name, $columns_to_return) {
                     global $wpdb;
-                    $results = $wpdb->get_results("SELECT " . implode(', ', $columns_to_return) . " FROM $table_name", ARRY_A);
-                    return $results;
+                    $results = $wpdb->get_results("SELECT " . implode(', ', $columns_to_return) . " FROM $table_name", ARRAY_A);
+                    return wp_send_json($results);
                 },
             ) );
         }
-
-        add_action('rest_api_init', 'register_custom_routes');
     }
 
-    public static function save_custom_route($route_name, $route_table, $route_columns, $route_method, $route_callback) {
+    public function save_custom_route($route_name, $route_table, $route_columns, $route_method, $route_callback) {
         global $wpdb;
         $table_name = $wpdb->prefix . 'custom_routes';
         $wpdb->insert($table_name, array(
