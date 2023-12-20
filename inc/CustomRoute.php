7ce194ca4f24a25fd09f914e4b1d17cbe6d9b956
recypher-zz <kniemerg001@me.com>
2023-12-20 14:19:23 -0800

    Added more logic and a case statment to determin what kind of request type is being made so that the callbacks are dynamic, if there is a better way I will investigate in the future.

--- inc/CustomRoute.php
+++ inc/CustomRoute.php
@@ -16,13 +16,92 @@ class CustomRoute{
             $table_name = $route['route_table'];
             $unserialize_columns = unserialize( $route['route_columns'] );
             $columns_to_return = $unserialize_columns;
+            $method = $route['route_method'];
 
             register_rest_route('apepi/v1', '/' . $route['route_name'], array(
                 'methods' => $route['route_method'],
-                'callback' => function ( $data ) use ($table_name, $columns_to_return) {
+                'callback' => function ( $data ) use ($table_name, $columns_to_return, $method) {
                     global $wpdb;
-                    $results = $wpdb->get_results("SELECT " . implode(', ', $columns_to_return) . " FROM $table_name", ARRAY_A);
-                    return wp_send_json($results);
+
+                    switch ($method){
+                        case "GET":
+                            $results = $wpdb->get_results("SELECT " . implode(', ', $columns_to_return) . " FROM $table_name", ARRAY_A);
+                            return wp_send_json($results);
+                            break;
+                        case "POST":
+                            $posted_data = $data->get_params();
+
+                            if (empty($posted_data)) {
+                                return rest_ensure_response(array('error' => 'Invalid request data'));
+                            }
+
+                            
+                            $columns = array_keys($posted_data);
+                            $values = array_values($posted_data);
+
+                            
+                            $columns = array_map('sanitize_text_field', $columns);
+                            $values = array_map('sanitize_text_field', $values);
+
+                            
+                            if (count($columns) !== count($values)) {
+                                return rest_ensure_response(array('error' => 'Mismatched number of columns and values'));
+                            }
+
+                            $sql_columns = implode(', ', $columns);
+                            $sql_values = implode("', '", $values);
+                            $sql = "INSERT INTO $table_name ($sql_columns) VALUES ('$sql_values')";
+
+                            $wpdb->query($sql);
+                            return rest_ensure_response(array('message' => 'Data inserted successfully'));
+                            break;
+                        case "PATCH":
+                            $posted_data = $data->get_params();
+
+                            if (empty($posted_data)) {
+                                return rest_ensure_response(array('error' => 'Invalid request...'));
+                            }
+
+                            $update_data = array();
+                            foreach ($posted_data as $key => $value) {
+
+                                $key = sanitize_text_field($key);
+                                $value = sanitize_text_field($value);
+
+                                $update_data[$key] = $value;
+                            }
+
+                            if (empty($update_data)) {
+                                return rest_ensure_response(array('error' => 'No valid data'));
+                            }
+
+                            $where = array();
+                            $columns = array_keys($posted_data);
+                            
+                            foreach ($columns as $column) {
+                                if (isset($posted_data[$column])) {
+                                    $where[] = $column . " = '" . sanitize_text_field($posted_data[$column]) . "'"; 
+                                }
+                            }
+
+                            if (empty($where)) {
+                                return rest_ensure_response(array('error' => 'No valid conditions'));
+                            }
+
+                            $sql_where = implode(' AND ', $where);
+                            $sql_update = implode(', ', array_map(function ($key, $value) {
+                                return $key . " = '" . $value . "'";
+                            }, array_keys($update_data), $update_data));
+
+                            $sql = "UPDATE $table_name SET $sql_update WHERE $sql_where";
+
+                            $wpdb->query($sql);
+                            break;
+                        case "DELETE":
+                            return "$method not supported yet";
+                            break;
+
+                    }
                 },
             ) );
         }
