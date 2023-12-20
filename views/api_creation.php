5bf2346bab83963d4a9d1c41c774a04f2d78d77e
recypher-zz <kniemerg001@me.com>
2023-12-20 14:19:37 -0800

    Added some Bootstrap styling to clean the page a little bit.

--- views/api_creation.php
+++ views/api_creation.php
@@ -2,33 +2,38 @@
 
 function api_creation_screen($data){
     ?>
-    <div>
-        <label for="route_name">Endpoint Name:</label>
-        <input class="api_name" name="route_name" type="text"> 
-    </div>
-    <div class="endpoint_selector">
-        <?php $data; ?>
-        <div id="returned_columns">
-
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <label for="route_name">Endpoint Name:</label>
+                <input class="api_name" name="route_name" type="text"> 
+            </div>
+            
         </div>
-        <div>
-            <select class="method_type">
-                <option value="#"> Select a Method... </option>
-                <option value="GET">GET</option>
-                <option value="POST">POST</option>
-                <option value="PATCH">PATCH</option>
-                <option value="DELETE">DELETE</option>
-            </select>
-        </div>
+        <div class="endpoint_selector">
+            <?php $data; ?>
+            <div id="returned_columns">
 
-        <div>
-            <pre class="returned_data">
+            </div>
+            <div>
+                <select class="method_type">
+                    <option value="#"> Select a Method... </option>
+                    <option value="GET">GET</option>
+                    <option value="POST">POST</option>
+                    <option value="PATCH">PATCH</option>
+                    <option value="DELETE">DELETE</option>
+                </select>
+            </div>
 
-            </pre>
-        </div>
+            <div>
+                <pre class="returned_data">
 
-        <div>
-            <button class="submit_api">Submit</button>
+                </pre>
+            </div>
+
+            <div>
+                <button class="submit_api">Submit</button>
+            </div>
         </div>
     </div>
 
