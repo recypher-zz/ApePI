3f874f50276b1d9625cc0098b2d5c2648ff6abd6
recypher-zz <kniemerg001@me.com>
2023-12-22 22:09:57 -0800

    Simplified the ListRoutes class to just return the data on function call.

--- inc/Components/ListRoutes.php
+++ inc/Components/ListRoutes.php
@@ -5,25 +5,9 @@ class ListRoutes {
 
     public static function list_routes(){
         global $wpdb;
-        $response = '';
-        $site_url = get_option('siteurl');
         $routes = $wpdb->get_results("SELECT * FROM " . $wpdb->prefix . 'custom_routes');
-
-        $registered_routes = [];
-
-        foreach ($routes as $route) {
-            $registered_routes[] = [
-                'name' => $route['route_name'],
-                'table' => $route['route_table'],
-                'columns' => $route['route_columns'],
-                'method' => $route['route_method']
-            ];
-        }
-
-        $response .= '<p>' . $site_url . '/apepi/v1/' . $route['name'] . '</p>';
-        $response .= '<p>Method: ' . $route['method'] . '</p>';
-
-        return wp_send_json($response);
+        
+        return $routes;
     }
 }
 
